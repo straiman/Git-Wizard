08f9f1dfff15dace857611d3f929f1578196266f
aramis <aramiscohen2@gmail.com>
2022-12-06 18:50:25 +0200

    Storage class with isStored method only

--- storage.js
+++ storage.js
@@ -5,22 +5,4 @@ class Storage {
         }
         return true;
     }
-
-    store(key, value) {
-        if(localStorage.key) {
-            const userInfo = JSON.parse(localStorage.getItem(key));
-            const finalObject = Object.assign(userInfo, value);
-            localStorage.setItem(key, JSON.stringify(finalObject));
-        } else {
-            localStorage.setItem(key, JSON.stringify(value));
-        }
-    }    
-
-    load(key) {
-        return JSON.parse(localStorage.getItem(key));
-    }
-
-    delete(key) {
-        localStorage.removeItem(key);
-    }
 }
