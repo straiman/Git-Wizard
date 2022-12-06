26d2668d8169d137146f904779dedd09deecd9dd
aramis <aramiscohen2@gmail.com>
2022-12-06 18:48:56 +0200

    Storage class with all the methods

---
+++ storage.js
@@ -0,0 +1,26 @@
+class Storage {
+    isStored(key) {
+        if (localStorage.getItem(key) === null) {
+            return false;
+        }
+        return true;
+    }
+
+    store(key, value) {
+        if(localStorage.key) {
+            const userInfo = JSON.parse(localStorage.getItem(key));
+            const finalObject = Object.assign(userInfo, value);
+            localStorage.setItem(key, JSON.stringify(finalObject));
+        } else {
+            localStorage.setItem(key, JSON.stringify(value));
+        }
+    }    
+
+    load(key) {
+        return JSON.parse(localStorage.getItem(key));
+    }
+
+    delete(key) {
+        localStorage.removeItem(key);
+    }
+}
