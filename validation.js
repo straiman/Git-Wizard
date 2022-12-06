bb5918543632ec349668ed79e8d3f2abd1cc99e4
GoncharikAnton <61192916+GoncharikAnton@users.noreply.github.com>
2022-12-06 18:22:39 +0200

    Skeleton Validation for forms.

--- validation.js
+++ validation.js
@@ -1,49 +1,18 @@
 class Validation{
-    constructor(){
-
-    }
 
     static isRequired(inp){
-        if(`${inp}`.length > 0){
-            return true;
-        }
-
-        return false;
     }
 
-    static nameValidation(val) {
-        const nameArr = val.split(' ');
-        console.log(nameArr)
-
-        if(nameArr.length < 2 || nameArr.length > 2){
-            return false
-        }
-        for (let i = 0; i < nameArr.length; i++) {
-            if(nameArr[i].length < 2){
-                return false;
-            }
-        }
-        return true;
+    static nameValidation(inp) {
     }
 
-    static emailValidation(val){
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val)) {
-            return true;
-        }
-        return false;
+    static emailValidation(inp){
     }
 
-    static lengthUpTo20ChValidation(val){
-        if(`${val}`.length <= 20){
-            return true;
-        }
-
-        return false;
+    static lengthValidation(inp, length){
     }
 
-    static naturalNumberValidation(val){
-        const pattern = /^(0|([1-9]\d*))$/;
-        return pattern.test(val);
+    static naturalNumberValidation(inp){
     }
 
 }
