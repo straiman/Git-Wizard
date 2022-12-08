9381f3d173d89b1a9a60528d6bc1c1a2b3843ad3
taliacohn <taliacohn3@gmail.com>
2022-12-08 14:48:23 +0200

    updates in styles

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,50 +1,50 @@
 class Storage {
-    constructor() {
-        this.key = 'wiz-user'
+  constructor() {
+    this.key = "wiz-user";
+  }
+  //This method check if the localstorage have some value with the key parameter
+  isStored() {
+    if (localStorage.getItem(this.key) === null) {
+      return false;
     }
-    //This method check if the localstorage have some value with the key parameter
-    isStored() {
-        if (localStorage.getItem(this.key) === null) {
-            return false;
-        }
-        return true;
-    }
+    return true;
+  }
 
-    //Method for save a value, into the localstorage
-    store(value) {
-        if(localStorage[`${this.key}`]) {
-            const userInfo = JSON.parse(localStorage.getItem(this));
-            const finalObject = Object.assign(userInfo, value);
-            localStorage.setItem(this.key, JSON.stringify(finalObject));
-        } else {
-            localStorage.setItem(this.key, JSON.stringify(value));
-        }
-    }    
-
-    //Method to load a value from the localstorage
-    load() {
-        return JSON.parse(localStorage.getItem(this.key));
+  //Method for save a value, into the localstorage
+  store(value) {
+    if (localStorage[`${this.key}`]) {
+      const userInfo = JSON.parse(localStorage.getItem(this));
+      const finalObject = Object.assign(userInfo, value);
+      localStorage.setItem(this.key, JSON.stringify(finalObject));
+    } else {
+      localStorage.setItem(this.key, JSON.stringify(value));
     }
+  }
 
-    //Method to delete informacion from the localstorage
-    delete() {
-        localStorage.removeItem(this.key);
-    }
+  //Method to load a value from the localstorage
+  load() {
+    return JSON.parse(localStorage.getItem(this.key));
+  }
 
-    //Method to return if a property already exists
-    checkProperty(property) {
-        const userInfo = JSON.parse(localStorage.getItem(this.key));
-        if(userInfo[property]) {
-            return true;
-        }
-        return false;
-    }
+  //Method to delete informacion from the localstorage
+  delete() {
+    localStorage.removeItem(this.key);
+  }
 
-    //Method to return a singular property
-    getProperty(property) {
-        const userInfo = JSON.parse(localStorage.getItem(this.key));
-        return userInfo[property];
+  //Method to return if a property already exists
+  checkProperty(property) {
+    const userInfo = JSON.parse(localStorage.getItem(this.key));
+    if (userInfo[property]) {
+      return true;
     }
+    return false;
+  }
+
+  //Method to return a singular property
+  getProperty(property) {
+    const userInfo = JSON.parse(localStorage.getItem(this.key));
+    return userInfo[property];
+  }
 }
 
 export default Storage;
